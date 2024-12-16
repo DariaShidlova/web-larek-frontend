350d759397c799c3fd8dbf9dee48b93497be3212
Daria Shidlova <daryashidlova@yandex.ru>
2024-12-17 02:04:06 +0300

    исправлены замечания

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
 import './scss/styles.scss';
-import { basketManager } from './types/basket';
-import { Catalog } from './types/catalog';
-import { Modal } from './types/modal';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const catalog = new Catalog('.gallery');
-    const basket = basketManager.getBasket();
-});
 
 
